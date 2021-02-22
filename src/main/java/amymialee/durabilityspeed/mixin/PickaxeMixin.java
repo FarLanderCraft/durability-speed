73c615f19266a9fdd659b3fb5fbf0b7057ea111e
AmyMialee <10999535+AmyMialee@users.noreply.github.com>
2021-02-22 12:14:50 +0000

    config

--- src/main/java/amymialee/durabilityspeed/mixin/PickaxeMixin.java
+++ src/main/java/amymialee/durabilityspeed/mixin/PickaxeMixin.java
@@ -1,5 +1,7 @@
 package amymialee.durabilityspeed.mixin;
 
+import amymialee.durabilityspeed.DurabilityModConfig;
+import me.shedaniel.autoconfig.AutoConfig;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Material;
@@ -20,11 +22,15 @@ public class PickaxeMixin extends MiningToolItem {
         super(attackDamage, attackSpeed, material, effectiveBlocks, settings);
     }
 
+    public DurabilityModConfig config = AutoConfig.getConfigHolder(DurabilityModConfig.class).getConfig();
+
     @Inject(method = "getMiningSpeedMultiplier", at = @At("RETURN"), cancellable = true)
     private void getMiningSpeedMultiplier(ItemStack stack, BlockState state, CallbackInfoReturnable<Float> cir) {
-        Material material = state.getMaterial();
-        float multiplier = 1;
-        multiplier = (1 - ((float) stack.getDamage() / (float) stack.getMaxDamage())) * 2;
-        cir.setReturnValue(material != Material.METAL && material != Material.REPAIR_STATION && material != Material.STONE  ? super.getMiningSpeedMultiplier(stack, state) : this.miningSpeed * multiplier );
+        if (config.effectPickaxes) {
+            Material material = state.getMaterial();
+            float multiplier = 1;
+            multiplier = ((1 - ((float) stack.getDamage() / (float) stack.getMaxDamage())) * (config.maximumSpeed - config.minimumSpeed)) + config.minimumSpeed;
+            cir.setReturnValue(material != Material.METAL && material != Material.REPAIR_STATION && material != Material.STONE ? super.getMiningSpeedMultiplier(stack, state) : this.miningSpeed * multiplier);
+        }
     }
 }
--- src/main/java/amymialee/durabilityspeed/mixin/ToolMixin.java
+++ src/main/java/amymialee/durabilityspeed/mixin/ToolMixin.java
@@ -1,9 +1,12 @@
 package amymialee.durabilityspeed.mixin;
 
+import amymialee.durabilityspeed.DurabilityModConfig;
+import me.shedaniel.autoconfig.AutoConfig;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.MiningToolItem;
+import net.minecraft.item.ShovelItem;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -19,10 +22,14 @@ public abstract class ToolMixin {
 
     @Shadow @Final protected float miningSpeed;
 
+    public DurabilityModConfig config = AutoConfig.getConfigHolder(DurabilityModConfig.class).getConfig();
+
     @Inject(method = "getMiningSpeedMultiplier", at = @At("RETURN"), cancellable = true)
     private void getMiningSpeedMultiplier(ItemStack stack, BlockState state, CallbackInfoReturnable<Float> cir) {
-        float multiplier = 1;
-        multiplier = (1 - ((float) stack.getDamage() / (float) stack.getMaxDamage())) * 2;
-        cir.setReturnValue(this.effectiveBlocks.contains(state.getBlock()) ? this.miningSpeed * multiplier : 1.0F);
+        if ((stack.getItem() instanceof ShovelItem && config.effectShovels) || (!(stack.getItem() instanceof ShovelItem) && config.effectOther)) {
+            float multiplier = 1;
+            multiplier = ((1 - ((float) stack.getDamage() / (float) stack.getMaxDamage())) * (config.maximumSpeed - config.minimumSpeed)) + config.minimumSpeed;
+            cir.setReturnValue(this.effectiveBlocks.contains(state.getBlock()) ? this.miningSpeed * multiplier : 1.0F);
+        }
     }
 }
